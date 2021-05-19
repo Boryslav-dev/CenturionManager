7fb66755efa4cce82c58467cb9caa7f1e3e769d4
Boryslav <kuchkovborislav000@gmail.com>
2021-05-19 20:27:42 +0300

    Create UserController with update user`s data method

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function update(Request $request){
+        $fields = $request->validate([
+            'id' => 'required',
+            'name' => 'string',
+            'email' => 'string',
+            'password' => 'required|string',
+            'flag_email' => 'string',
+        ]);
+
+        $user = User::where('id', $fields['id'])->first();
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        //Change email
+        if($fields['flag_email'] == '1') {
+            $user = User::where('email', $fields['email'])->first();
+            //Check email
+            if ($user !== null) {
+                return response([
+                    'message' => 'email was exists'
+                ], 401);
+            }
+            else{
+                User::where(['id' => $fields['id']])->update([
+                    'email' => $fields['email'],
+                ]);
+            }
+        }
+
+        User::where(['id' => $fields['id']])->update([
+            'name' => $fields['name'],
+        ]);
+
+        $user = User::where(['id' => $fields['id']])->first();
+
+        $response = [
+            'user' => $user,
+        ];
+
+        return response($response, 201);
+    }
+}
